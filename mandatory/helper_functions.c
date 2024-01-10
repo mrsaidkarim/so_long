249e22a26240f4745c4763767f4c21ed6cc6d187
SaidKarim-web <sirsaidkarim@gmail.com>
2024-01-10 13:20:02 +0100

    Modify the helper_functions file

--- mandatory/helper_functions.c
+++ mandatory/helper_functions.c
@@ -6,7 +6,7 @@
 /*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 18:53:06 by skarim            #+#    #+#             */
-/*   Updated: 2024/01/08 19:01:02 by skarim           ###   ########.fr       */
+/*   Updated: 2024/01/10 11:51:58 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,24 @@ void	ft_putnbr(int n)
 		ft_putnbr(n / 10);
 		ft_putchar((n % 10) + '0');
 	}
+}
+
+char	*ft_strdup(char *s1)
+{
+	char	*res;
+	int		size;
+	int		i;
+
+	size = ft_strlen(s1);
+	res = (char *)malloc((size + 1));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
