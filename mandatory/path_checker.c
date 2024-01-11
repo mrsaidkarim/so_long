b1a5e1435d14f74023e7b8a67377a0c9656a68b4
SaidKarim-web <sirsaidkarim@gmail.com>
2024-01-11 09:50:10 +0100

    Modify ft_flood_fill function

--- mandatory/path_checker.c
+++ mandatory/path_checker.c
@@ -6,11 +6,11 @@
 /*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:02:42 by skarim            #+#    #+#             */
-/*   Updated: 2024/01/10 12:03:19 by skarim           ###   ########.fr       */
+/*   Updated: 2024/01/11 09:44:00 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#include "../so_long.h"
 
 char	**ft_copy_map(t_map m)
 {
@@ -30,21 +30,26 @@ char	**ft_copy_map(t_map m)
 	return (res);
 }
 
-void	ft_flood_fill(char **copy_map, int *ptr_coins, int *ptr_exit, int x_p, int y_p)
+void	ft_flood_fill(char **copy_map, int *ptr_coins, int *ptr_exit,
+	t_player p)
 {
-	if (copy_map[y_p][x_p] == 'E')
+	if (copy_map[p.y][p.x] == 'E')
 		*ptr_exit = 1;
-	if (copy_map[y_p][x_p] == 'C')
+	if (copy_map[p.y][p.x] == 'C')
 		*ptr_coins += 1;
-	copy_map[y_p][x_p] = '.';
-	if (copy_map[y_p - 1][x_p] != '1' && copy_map[y_p - 1][x_p] != '.')
-		ft_flood_fill(copy_map, ptr_coins, ptr_exit, x_p, (y_p - 1));
-	if (copy_map[y_p][x_p - 1] != '1' && copy_map[y_p][x_p - 1] != '.')
-		ft_flood_fill(copy_map, ptr_coins, ptr_exit, (x_p - 1), y_p);
-	if (copy_map[y_p + 1][x_p] != '1' && copy_map[y_p + 1][x_p] != '.')
-		ft_flood_fill(copy_map, ptr_coins, ptr_exit, x_p, (y_p + 1));
-	if (copy_map[y_p][x_p + 1] != '1' && copy_map[y_p][x_p + 1] != '.')
-		ft_flood_fill(copy_map, ptr_coins, ptr_exit, (x_p + 1), y_p);
+	copy_map[p.y][p.x] = '.';
+	if (copy_map[p.y - 1][p.x] != '1' && copy_map[p.y - 1][p.x] != '.')
+		ft_flood_fill(copy_map, ptr_coins, ptr_exit,
+			(t_player){p.x, p.y - 1, p.direction, p.mv_nbr});
+	if (copy_map[p.y][p.x - 1] != '1' && copy_map[p.y][p.x - 1] != '.')
+		ft_flood_fill(copy_map, ptr_coins, ptr_exit,
+			(t_player){p.x - 1, p.y, p.direction, p.mv_nbr});
+	if (copy_map[p.y + 1][p.x] != '1' && copy_map[p.y + 1][p.x] != '.')
+		ft_flood_fill(copy_map, ptr_coins, ptr_exit,
+			(t_player){p.x, p.y + 1, p.direction, p.mv_nbr});
+	if (copy_map[p.y][p.x + 1] != '1' && copy_map[p.y][p.x + 1] != '.')
+		ft_flood_fill(copy_map, ptr_coins, ptr_exit,
+			(t_player){p.x + 1, p.y, p.direction, p.mv_nbr});
 }
 
 void	ft_check_path(t_game **g)
@@ -57,7 +62,7 @@ void	ft_check_path(t_game **g)
 	coins_accessible = 0;
 	exit = 0;
 	copy_map = ft_copy_map((*g)->map);
-	ft_flood_fill(copy_map, &coins_accessible, &exit, (*g)->player.x, (*g)->player.y);
+	ft_flood_fill(copy_map, &coins_accessible, &exit, (*g)->player);
 	if (exit == 0)
 		ft_maperror(g, "Invalid Map: The exit is not reachable.");
 	if (coins_accessible != (*g)->map.coins)
