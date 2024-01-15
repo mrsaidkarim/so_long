703bbada4fc9accd727379e07aef843c3cdb4561
SaidKarim-web <sirsaidkarim@gmail.com>
2024-01-15 21:11:03 +0100

    Add some helper functions

---
+++ bonus/helper_functions_bonus.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helper_functions_bonus.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 15:08:02 by skarim            #+#    #+#             */
+/*   Updated: 2024/01/15 15:08:36 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n < 10)
+		ft_putchar(n + '0');
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putchar((n % 10) + '0');
+	}
+}
+
+char	*ft_strdup(char *s1)
+{
+	char	*res;
+	int		size;
+	int		i;
+
+	size = ft_strlen(s1);
+	res = (char *)malloc((size + 1));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
