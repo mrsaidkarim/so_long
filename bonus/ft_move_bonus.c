62403891dba8f82da6f6b3d6087c0a013914c6c4
SaidKarim-web <sirsaidkarim@gmail.com>
2024-01-19 22:12:25 +0100

    Add move_player function

---
+++ bonus/ft_move_bonus.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_move_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 15:45:08 by skarim            #+#    #+#             */
+/*   Updated: 2024/01/16 12:01:08 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+void	ft_move_left(t_game *g, int x, int y)
+{
+	g->player.direction = "left";
+	if (g->map.content[y][x - 1] != '1')
+	{
+		if (g->map.content[y][x - 1] == 'E' && g->map.coins != 0)
+			return ;
+		if (g->map.content[y][x - 1] == 'E' && g->map.coins == 0)
+		{
+			write(1, "You Won!", 8);
+			ft_free_game(g);
+		}
+		if (g->map.content[y][x - 1] == 'C')
+			g->map.coins--;
+		g->map.content[y][x] = '0';
+		g->map.content[y][x - 1] = 'P';
+		ft_put_image_to_window(g, g->floor_xpm, x, y);
+		ft_put_image_to_window(g, g->pac_man_lo_xpm, x - 1, y);
+		g->player.x -= 1;
+		g->player.mv_nbr++;
+	}
+}
+
+void	ft_move_right(t_game *g, int x, int y)
+{
+	g->player.direction = "right";
+	if (g->map.content[y][x + 1] != '1')
+	{
+		if (g->map.content[y][x + 1] == 'E' && g->map.coins != 0)
+			return ;
+		if (g->map.content[y][x + 1] == 'E' && g->map.coins == 0)
+		{
+			write(1, "You Won!", 8);
+			ft_free_game(g);
+		}
+		if (g->map.content[y][x + 1] == 'C')
+			g->map.coins--;
+		g->map.content[y][x] = '0';
+		g->map.content[y][x + 1] = 'P';
+		ft_put_image_to_window(g, g->floor_xpm, x, y);
+		ft_put_image_to_window(g, g->pac_man_ro_xpm, x + 1, y);
+		g->player.x += 1;
+		g->player.mv_nbr++;
+	}
+}
+
+void	ft_move_up(t_game *g, int x, int y)
+{
+	g->player.direction = "up";
+	if (g->map.content[y - 1][x] != '1')
+	{
+		if (g->map.content[y - 1][x] == 'E' && g->map.coins != 0)
+			return ;
+		if (g->map.content[y - 1][x] == 'E' && g->map.coins == 0)
+		{
+			write(1, "You Won!", 8);
+			ft_free_game(g);
+		}
+		if (g->map.content[y - 1][x] == 'C')
+			g->map.coins--;
+		g->map.content[y][x] = '0';
+		g->map.content[y - 1][x] = 'P';
+		ft_put_image_to_window(g, g->floor_xpm, x, y);
+		ft_put_image_to_window(g, g->pac_man_uo_xpm, x, y - 1);
+		g->player.y -= 1;
+		g->player.mv_nbr++;
+	}
+}
+
+void	ft_move_down(t_game *g, int x, int y)
+{
+	g->player.direction = "down";
+	if (g->map.content[y + 1][x] != '1')
+	{
+		if (g->map.content[y + 1][x] == 'E' && g->map.coins != 0)
+			return ;
+		if (g->map.content[y + 1][x] == 'E' && g->map.coins == 0)
+		{
+			write(1, "You Won!", 8);
+			ft_free_game(g);
+		}
+		if (g->map.content[y + 1][x] == 'C')
+			g->map.coins--;
+		g->map.content[y][x] = '0';
+		g->map.content[y + 1][x] = 'P';
+		ft_put_image_to_window(g, g->floor_xpm, x, y);
+		ft_put_image_to_window(g, g->pac_man_do_xpm, x, y + 1);
+		g->player.y += 1;
+		g->player.mv_nbr++;
+	}
+}
+
+int	ft_move_player(int keycode, t_game *g)
+{
+	int	x;
+	int	y;
+
+	x = g->player.x;
+	y = g->player.y;
+	if ((keycode == 123 || keycode == 0))
+		ft_move_left(g, x, y);
+	if ((keycode == 124 || keycode == 2))
+		ft_move_right(g, x, y);
+	if ((keycode == 126 || keycode == 13))
+		ft_move_up(g, x, y);
+	if ((keycode == 125 || keycode == 1))
+		ft_move_down(g, x, y);
+	if (keycode == 53)
+		ft_free_game(g);
+	ft_put_moves(g);
+	return (0);
+}
